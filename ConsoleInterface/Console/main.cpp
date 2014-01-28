f65ffbd8e9556ac12c296b24ad37fc3050c7567b
RomanDengin <dengin.ru@outlook.com>
2014-01-29 00:34:55 +0600

    Realized reading number of command

--- ConsoleInterface/Console/main.cpp
+++ ConsoleInterface/Console/main.cpp
@@ -3,20 +3,21 @@ using namespace std;
 
 int get_num()
 {
-	int n;
+	char n;
 	cin >> n;
 	return n;
 }
 
 void menu()
 {
-	//maximum number is 5
-	int n = get_num();	//Get a number of command. DOES NOT WORK, IF AN INPUT IS NOT A NUMBER!
-	cout << n << endl;
-	if (n<1 || n>5)
+	char n = get_num();
+
+
+	//Read number of command
+	if (n<'1' || n>'5')
 	{
 		cout << "You have entered wrong number. Please, enter a number from 1 to 5." << endl;
-		get_num();
+		menu();
 	}
 }
 
